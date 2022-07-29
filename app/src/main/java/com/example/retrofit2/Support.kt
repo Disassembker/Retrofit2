f34b6f830f65469f9279877342faeebe06dd48c0
Notebook <iva77@mail.ru>
2022-07-29 15:35:57 +0300

    First attempt

---
+++ app/src/main/java/com/example/retrofit2/Support.kt
@@ -0,0 +1,10 @@
+package com.example.retrofit2
+
+import com.google.gson.annotations.SerializedName
+
+data class Support(
+    @SerializedName("text")
+    var text: String,
+    @SerializedName("url")
+    var url: String
+)
